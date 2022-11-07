8c3db67b83bc0b594d5955b05c0c7656cfa8eba8
Oleksandr Korenivskyi <sana22_koo@student.ztu.edu.ua>
2022-11-07 13:41:28 +0300

    First three phrases changed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,9 +6,9 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+            Console.WriteLine("I love lo-fi music");
+            Console.WriteLine("I love lo-fi music");
+            Console.WriteLine("I love lo-fi music");
             Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
             Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
             Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
