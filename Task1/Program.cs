56bb99ca8458abde7640f3067c55ef50738aee0d
Oleksandr Korenivskyi <sana22_koo@student.ztu.edu.ua>
2022-11-07 13:44:03 +0300

    Last two phrases changed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,8 +10,8 @@ namespace Task1
             Console.WriteLine("I love lo-fi music");
             Console.WriteLine("I love lo-fi music");
             Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+            Console.WriteLine("I also like ambient music");
+            Console.WriteLine("I also like ambient music");
         }
     }
 }
